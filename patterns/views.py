e5c4ad4be5b2e88f26e58a567d3450bb0d5a265d
cameronmclean <ca.mclean@auckland.ac.nz>
2014-05-07 18:27:15 +1200

    view now adds context and problem to pattern added on previous page but needs work - errors from patern name/pictogram are passing silently and we are redirecting to add problem/contex - but the session pk is not updated because the save did not complete - resulting in  adding the new prob/context to the *previous* pattern...

--- patterns/views.py
+++ patterns/views.py
@@ -16,9 +16,12 @@ def add_new_pattern_name(request):
 	form = NewPatternName()
 	if request.method == 'POST':
 		form = NewPatternName(request.POST, request.FILES)
+		# if form is valid save it to a new object, and store the object contents in session dictonary
 		if form.is_valid():
-			newPatternInstance = form.save(commit=False)
-		
+			newPatternInstance = form.save()
+			request.session['new_pattern_key'] = newPatternInstance.id
+			request.session['new_pattern_name'] = newPatternInstance.name
+	
 		return redirect('/newprobtext/')
 
 	return render(request, 'new_name.html', {'form':form})
@@ -33,16 +36,15 @@ def add_new_prob_and_context(request):
 		formP = NewProblem(request.POST)
 		formC = NewContext(request.POST)
 		if formP.is_valid() and formC.is_valid():
+			# save form to object
 			newProblemInstance = formP.save(commit=False)
 			newContextInstance = formC.save(commit=False)
-			newProblemInstance.parent_pattern = add_new_pattern_name.newPatternInstance.id 
-			newContextInstance.partent_pattern = add_new_pattern_name.newPatternInstance.id
-
-			newPatternInstance.save()
-			newContextInstance.save()
+			# give the object the last created pattern ID key
+			newProblemInstance.parent_pattern = DesignPattern.objects.get(id = request.session['new_pattern_key'])
+			newContextInstance.parent_pattern = DesignPattern.objects.get(id = request.session['new_pattern_key'])
+			# save the objects into the db
 			newProblemInstance.save()
-
-	
+			newContextInstance.save()	
 		return redirect('/')
 	
 	return render(request, 'new_probtext.html', {'formP':formP, 'formC':formC})
