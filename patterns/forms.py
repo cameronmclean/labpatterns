9d3aab0ce85c0f56f56a748e0f9e83f5634de837
cameronmclean <ca.mclean@auckland.ac.nz>
2014-05-07 16:36:15 +1200

    update to problem/context views - not working yet though need to pass variables between views and save all forms/objects to db at end

--- patterns/forms.py
+++ patterns/forms.py
@@ -1,10 +1,24 @@
-from django.forms import ModelForm, TextInput
-from patterns.models import DesignPattern
+from django.forms import ModelForm, TextInput, HiddenInput
+from patterns.models import *
 
+# This is the form to add a new pattern name and pattern pictogram to the database - it is inteded as the fist page for adding a new pattern.
 class NewPatternName(ModelForm):
 	class Meta:
 		model = DesignPattern 
 		fields = ['name', 'pictogram']
-		# need to add pictogram to form - but modelForm and model have a field mismatch that needs to be declared or else validation errors ensue.
 		widgets = { 'name': TextInput(attrs={'placeholder': 'Enter a pretentious soundign name for the pattern'})
+		}
+
+class NewProblem(ModelForm):
+	class Meta:
+		model = Problem 
+		fields = ['description']
+		widgets = { 'description': TextInput(attrs={'placeholder': 'Enter a pithy description of the problem'})
+		}
+
+class NewContext(ModelForm):
+	class Meta:
+		model = Context 
+		fields = ['description']
+		widgets = { 'description': TextInput(attrs={'placeholder': 'Enter a detailed description of the context in which this problem occurs'})
 		}
--- patterns/views.py
+++ patterns/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from patterns.forms import NewPatternName
+from patterns.forms import *
 
 
 # Create your views here.
 
+
+# The home page 
 def home(request):
 	return render(request, 'home.html')
 
+
+# The first page to add a new pattern - after successful form submision it should redirect to adding problem and context
 def add_new_pattern_name(request):
 	form = NewPatternName()
 	if request.method == 'POST':
 		form = NewPatternName(request.POST, request.FILES)
 		if form.is_valid():
-			# model ImageField and modelForms field are causing validation errors. Need to declare the right type.. 
-			form.save()
+			newPatternInstance = form.save(commit=False)
 		
-		return redirect('/')
+		return redirect('/newprobtext/')
 
 	return render(request, 'new_name.html', {'form':form})
+
+
+# the next page to add problem and context to a new pattern
+# the template will display the just added name and picture... 
+def add_new_prob_and_context(request):
+	formP = NewProblem()
+	formC = NewContext()
+	if request.method == 'POST':
+		formP = NewProblem(request.POST)
+		formC = NewContext(request.POST)
+		if formP.is_valid() and formC.is_valid():
+			newProblemInstance = formP.save(commit=False)
+			newContextInstance = formC.save(commit=False)
+			newProblemInstance.parent_pattern = add_new_pattern_name.newPatternInstance.id 
+			newContextInstance.partent_pattern = add_new_pattern_name.newPatternInstance.id
+
+			newPatternInstance.save()
+			newContextInstance.save()
+			newProblemInstance.save()
+
+	
+		return redirect('/')
+	
+	return render(request, 'new_probtext.html', {'formP':formP, 'formC':formC})
