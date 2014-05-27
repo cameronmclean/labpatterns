c83dc3a3a10df1e904dcd515714a290197c03e2a
cameronmclean <ca.mclean@auckland.ac.nz>
2014-05-27 15:49:00 +1200

    ran South to do the schema migration on WorkshopMetadata and PatternRelation

---
+++ patterns/migrations/0009_auto__add_workshopmetadata__add_patternrelation.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'WorkshopMetadata'
+        db.create_table(u'workshop_materials', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('parent_pattern', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['patterns.DesignPattern'])),
+            ('media', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+        ))
+        db.send_create_signal(u'patterns', ['WorkshopMetadata'])
+
+        # Adding model 'PatternRelation'
+        db.create_table(u'pattern_relations', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('subject_pattern', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'subject_pattern', to=orm['patterns.DesignPattern'])),
+            ('linked_pattern', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'linked_pattern', to=orm['patterns.DesignPattern'])),
+            ('relationship', self.gf('django.db.models.fields.TextField')(default=u'lp:relatedTo', max_length=50)),
+        ))
+        db.send_create_signal(u'patterns', ['PatternRelation'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'WorkshopMetadata'
+        db.delete_table(u'workshop_materials')
+
+        # Deleting model 'PatternRelation'
+        db.delete_table(u'pattern_relations')
+
+
+    models = {
+        u'patterns.context': {
+            'Meta': {'object_name': 'Context', 'db_table': "u'context'"},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"})
+        },
+        u'patterns.designpattern': {
+            'Meta': {'object_name': 'DesignPattern', 'db_table': "u'design_pattern'"},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '45', 'blank': 'True'}),
+            'contributor': ('django.db.models.fields.CharField', [], {'max_length': '45', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'pictogram': ('django.db.models.fields.files.FileField', [], {'max_length': '100'})
+        },
+        u'patterns.diagram': {
+            'Meta': {'object_name': 'Diagram', 'db_table': "u'diagram'"},
+            'comment': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'diagram': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'patterns.evidence': {
+            'Meta': {'object_name': 'Evidence', 'db_table': "u'evidence'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"}),
+            'reference': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        u'patterns.force': {
+            'Meta': {'object_name': 'Force', 'db_table': "u'pattern_force'"},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"}),
+            'pictogram': ('django.db.models.fields.files.FileField', [], {'max_length': '100'})
+        },
+        u'patterns.patternrelation': {
+            'Meta': {'object_name': 'PatternRelation', 'db_table': "u'pattern_relations'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'linked_pattern': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'linked_pattern'", 'to': u"orm['patterns.DesignPattern']"}),
+            'relationship': ('django.db.models.fields.TextField', [], {'default': "u'lp:relatedTo'", 'max_length': '50'}),
+            'subject_pattern': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'subject_pattern'", 'to': u"orm['patterns.DesignPattern']"})
+        },
+        u'patterns.problem': {
+            'Meta': {'object_name': 'Problem', 'db_table': "u'problem'"},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"})
+        },
+        u'patterns.rationale': {
+            'Meta': {'object_name': 'Rationale', 'db_table': "u'rationale'"},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"})
+        },
+        u'patterns.relatedontologyterm': {
+            'Meta': {'object_name': 'RelatedOntologyTerm', 'db_table': "u'ontology_terms'"},
+            'definition': ('django.db.models.fields.TextField', [], {}),
+            'force': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.Force']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ontology': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'prefLabel': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'relationship': ('django.db.models.fields.CharField', [], {'default': "u'skos:relatedMatch'", 'max_length': '25'}),
+            'synonyms': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'term': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'patterns.relatedword': {
+            'Meta': {'object_name': 'RelatedWord', 'db_table': "u'force_words'"},
+            'force': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.Force']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'word': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        u'patterns.solution': {
+            'Meta': {'object_name': 'Solution', 'db_table': "u'solution'"},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"})
+        },
+        u'patterns.workshopmetadata': {
+            'Meta': {'object_name': 'WorkshopMetadata', 'db_table': "u'workshop_materials'"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'media': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'parent_pattern': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patterns.DesignPattern']"})
+        }
+    }
+
+    complete_apps = ['patterns']
